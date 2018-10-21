37174a20842ce126fed77a0e3f3006d13d434665
laurenalyn <laurenalynwilkerson@gmail.com>
2018-10-21 16:04:49 -0400

    Initial file setup

---
+++ JavaScriptBasics/index.js
@@ -0,0 +1,13 @@
+// Primitave Data types:
+    // strings ("lkjklj")
+    // numbers (23)
+    // booleans (true / false)
+    // undefinded (no value)
+    // null (does not exist)
+    let firstName = 'John';
+    let lastName = 'Smith';
+    let age = 28;
+    let fullAge = true;
+    let job;  //undefined variable   
+    job = 'Teacher'; // defines job variable
+    
